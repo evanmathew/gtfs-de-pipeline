8611915b803d9a90ae7739fe25b51faf2a3a143c
evanmathew <evanptc@gmail.com>
2026-09-19 11:37:34 +0530

    updated schema file for CDC

--- sql/ddl/001_create_gtfs_schema.sql
+++ sql/ddl/001_create_gtfs_schema.sql
@@ -10,3 +10,5 @@ GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO transit_app;
 CREATE ROLE debezium_replicator WITH LOGIN PASSWORD 'deb_rpl_pass_4321' REPLICATION;
 GRANT CONNECT ON DATABASE nyc_transit_oltp TO debezium_replicator;
 
+GRANT SELECT ON vehicle_current_state TO debezium_replicator;
+CREATE PUBLICATION dbz_publication FOR TABLE vehicle_current_state;
